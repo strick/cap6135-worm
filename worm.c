4676c41c6c571cc18d4b992a04a6dad59ad94e47
Brian <bstrick@gmail.com>
2021-03-31 18:37:30 -0400

    Succesfully init the network

--- worm.c
+++ worm.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
+typedef enum 
+{
+    immune,
+    susceptible,
+    infectious
+} nodeType;
+
 #define patientZero 1001 // Where the infection starts!
 #define networkOmega 100000 // The number of systems in the network
 #define scanRate 3  // The number of scans an infected computer can do
 #define simulationN 3 // The number of simulations we want to run
 #define simulationTime 3000  // How many times the simulation might run to infect all nodes
 
-enum nodeType
-{
-    immune,
-    susceptible,
-    infectious
-};
+void build_network(nodeType NodeStatus[]);
+int network_is_fully_infected();
 
 // Create an array of the node types so you can model the network.
-enum nodeType NodeStatus[networkOmega + 1];
-enum nodeType PriorNodeStatus[networkOmega + 1];
+nodeType NodeStatus[networkOmega + 1];
+nodeType PriorNodeStatus[networkOmega + 1];
 
 int lt[simulationN][simulationTime];
 
@@ -23,6 +26,14 @@ int main(void)
 {
     // The number of infected computers.
     int infectedComputers = 1;
+
+    // Build the network
+    build_network(NodeStatus);
+
+    for(int i = 1; i<=1000; i++)
+    {
+        //printf("IP: %d is %d\n", i, NodeStatus[i]);
+    }
 
     // While there are still susceptible computers in the network, try to infect more.
     while(!network_is_fully_infected())
@@ -34,9 +45,25 @@ int main(void)
 }
 
 // Build a network  of 100,000 network nodes with IP addresses from 1 - 100,000
-void build_network()
+void build_network(nodeType NodeStatus[])
 {
+    for(int i = 1; i<=networkOmega; i++)
+    {
+        NodeStatus[i] = immune;
+    }
 
+    for(int j = 0; j<=99; j++)
+    {
+        for(int i = 0; i<=10; i++)
+        {
+            // Don't touch teh 1000, 2000, etc
+            if((i+j*1000)%1000 != 0)
+            {
+                NodeStatus[i+j*1000] = susceptible;
+                //printf("IP: %d is %d\n", i+j*1000, NodeStatus[i]);
+            }            
+        }
+    }
 }
 
 // Initialize the network node types
@@ -56,7 +83,7 @@ void infect_computer()
 
 }
 
-bool network_is_fully_infected()
+int network_is_fully_infected()
 {
-    return false;
+    return 0;
 }
